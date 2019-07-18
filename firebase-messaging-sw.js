6ae54ef6e2cbcc955e2b54fda77f6e35ea465adf
cs007good <53036223+cs007good@users.noreply.github.com>
2019-07-18 15:29:15 +0800

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+var config = {
+    messagingSenderId: "765573338074"
+}
+
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('Received background message ', payload);
+    if (Notification.permission === 'granted') {
+        notification = new Notification(payload.data.title, {
+            icon: '/icon/ms-icon-310x310.png',
+            body: payload.data.body,
+            onclick: function () {
+                parent.focus();
+                window.focus(); //just in case, older browsers
+                this.close();
+            }
+        })
+        //三秒後自動關閉
+        setTimeout(notification.close.bind(notification), 3000);
+    }
+});
